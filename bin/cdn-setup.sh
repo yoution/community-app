795a6471c47db5e265ed932655072b90b63755ab
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2018-05-01 01:02:14 +0200

    Invalidation of CDN cache for static assets

--- bin/cdn-setup.sh
+++ bin/cdn-setup.sh
@@ -11,9 +11,19 @@ create() {
   aws cloudfront create-distribution --distribution-config file://$PWD/config/aws/$3
 }
 
-echo "Development public CDN distribution"
-create $DEV_AWS_ACCESS_KEY_ID $DEV_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-dev.json
+# echo "Development public CDN distribution"
+# create $DEV_AWS_ACCESS_KEY_ID $DEV_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-dev.json
 
-echo "Producton public CDN distribution"
-create $PROD_AWS_ACCESS_KEY_ID $PROD_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-prod.json
+# echo "Producton public CDN distribution"
+# create $PROD_AWS_ACCESS_KEY_ID $PROD_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-prod.json
 
+# Invalidates some of the cached stuff
+invalidate() {
+  aws configure set aws_access_key_id $1
+  aws configure set aws_secret_access_key $2
+  aws configure set preview.cloudfront true
+  aws cloudfront create-invalidation --distribution-id $3 --paths "/static-assets/*"
+}
+
+invalidate $DEV_AWS_ACCESS_KEY_ID $DEV_AWS_SECRET_ACCESS_KEY E1N7Y5O368XHVT
+invalidate $PROD_AWS_ACCESS_KEY_ID $PROD_AWS_SECRET_ACCESS_KEY EKD44KXC98KGN
