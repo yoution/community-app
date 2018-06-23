575a3d9d5badfdeba95535d645c724d8bcc943e2
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2018-06-23 17:33:37 +0200

    Update of CDN config: decreases 404 error caching to 1 second
    This is a quick workaround for re-deployment problems caused by multiple
    instances of the app (both old and new) being running behind the load
    balancer for a short period of time. Default error caching was 5
    minutes.
    
    Relates to #1077

--- bin/cdn-setup.sh
+++ bin/cdn-setup.sh
@@ -17,6 +17,17 @@ create() {
 # echo "Producton public CDN distribution"
 # create $PROD_AWS_ACCESS_KEY_ID $PROD_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-prod.json
 
+# Updates distribution
+update() {
+  aws configure set aws_access_key_id $1
+  aws configure set aws_secret_access_key $2
+  aws configure set preview.cloudfront true
+  aws cloudfront update-distribution --distribution-config file://$PWD/config/aws/$3 --id $4
+}
+
+update $DEV_AWS_ACCESS_KEY_ID $DEV_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-dev.json E1N7Y5O368XHVT
+update $PROD_AWS_ACCESS_KEY_ID $PROD_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-prdo.json EKD44KXC98KGN
+
 # Invalidates some of the cached stuff
 invalidate() {
   aws configure set aws_access_key_id $1
@@ -25,5 +36,6 @@ invalidate() {
   aws cloudfront create-invalidation --distribution-id $3 --paths "/static-assets/*"
 }
 
-invalidate $DEV_AWS_ACCESS_KEY_ID $DEV_AWS_SECRET_ACCESS_KEY E1N7Y5O368XHVT
-invalidate $PROD_AWS_ACCESS_KEY_ID $PROD_AWS_SECRET_ACCESS_KEY EKD44KXC98KGN
+# invalidate $DEV_AWS_ACCESS_KEY_ID $DEV_AWS_SECRET_ACCESS_KEY E1N7Y5O368XHVT
+# invalidate $PROD_AWS_ACCESS_KEY_ID $PROD_AWS_SECRET_ACCESS_KEY EKD44KXC98KGN
+
