e76348dcd800b5a83d6f65b5583801498a4f283e
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2018-06-23 17:36:26 +0200

    Fix of CDN config update script

--- bin/cdn-setup.sh
+++ bin/cdn-setup.sh
@@ -22,11 +22,11 @@ update() {
   aws configure set aws_access_key_id $1
   aws configure set aws_secret_access_key $2
   aws configure set preview.cloudfront true
-  aws cloudfront update-distribution --distribution-config file://$PWD/config/aws/$3 --id $4
+  aws cloudfront update-distribution --distribution-config file://$PWD/config/aws/$3 --id $4 --if-match $5
 }
 
-update $DEV_AWS_ACCESS_KEY_ID $DEV_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-dev.json E1N7Y5O368XHVT
-update $PROD_AWS_ACCESS_KEY_ID $PROD_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-prdo.json EKD44KXC98KGN
+update $DEV_AWS_ACCESS_KEY_ID $DEV_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-dev.json E1N7Y5O368XHVT E3EQ3ONI4KCYR
+update $PROD_AWS_ACCESS_KEY_ID $PROD_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-prod.json EKD44KXC98KGN E1VW2GF4WR8A1K
 
 # Invalidates some of the cached stuff
 invalidate() {
