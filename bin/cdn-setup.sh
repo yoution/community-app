de87d3d1880608a3eca8043e10409dc78e0fa6da
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2018-06-23 17:56:46 +0200

    Gets current CDN configs

--- bin/cdn-setup.sh
+++ bin/cdn-setup.sh
@@ -17,6 +17,17 @@ create() {
 # echo "Producton public CDN distribution"
 # create $PROD_AWS_ACCESS_KEY_ID $PROD_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-prod.json
 
+# Gets distribution config
+get_config() {
+  aws configure set aws_access_key_id $1
+  aws configure set aws_secret_access_key $2
+  aws configure set preview.cloudfront true
+  aws cloudfront get-distribution-config --id $3
+}
+
+get_config $DEV_AWS_ACCESS_KEY_ID $DEV_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-dev.json E1N7Y5O368XHVT
+get_config $PROD_AWS_ACCESS_KEY_ID $PROD_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-prod.json EKD44KXC98KGN
+
 # Updates distribution
 update() {
   aws configure set aws_access_key_id $1
@@ -25,8 +36,8 @@ update() {
   aws cloudfront update-distribution --distribution-config file://$PWD/config/aws/$3 --id $4 --if-match $5
 }
 
-update $DEV_AWS_ACCESS_KEY_ID $DEV_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-dev.json E1N7Y5O368XHVT E3EQ3ONI4KCYR
-update $PROD_AWS_ACCESS_KEY_ID $PROD_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-prod.json EKD44KXC98KGN E1VW2GF4WR8A1K
+# update $DEV_AWS_ACCESS_KEY_ID $DEV_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-dev.json E1N7Y5O368XHVT E3EQ3ONI4KCYR
+# update $PROD_AWS_ACCESS_KEY_ID $PROD_AWS_SECRET_ACCESS_KEY cloudfront-public-distribution-prod.json EKD44KXC98KGN E1VW2GF4WR8A1K
 
 # Invalidates some of the cached stuff
 invalidate() {
